81207bfc170d043f5951be89aca2f66d0eb65262
fancy164 <66501889+fancy164@users.noreply.github.com>
2020-06-20 19:14:23 +0800

    Step one

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on("ready", function (){
+  console.log('${bot.user.username} Is Online!');
+  bot.user.setActivity('Titah Kaisar Fancy', {type: "LISTENING"}).catch(console.error);
+});
+
+bot.login(process.env.token);
