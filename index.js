d12a71230d20265c13b42998c55a27779b38257b
fancy164 <66501889+fancy164@users.noreply.github.com>
2020-06-20 19:39:14 +0800

    ADD BLACKSLIST

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ bot.on("ready", function (){
   bot.user.setActivity('Titah Kaisar Fancy', {type: "LISTENING"}).catch(console.error);
 });
 
+bot.on('message', message => {
+
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  
+  
+  let blacklisted = ['bangsat', 'bangsad', 'bgsd', 'bgst', 'bangst', 'bngsat', 'anjing', 'njing', 'anying', 'anjng', 'goblok', 'gblk'];
+  let foundInText = false;
+  for (var i in blacklisted){
+    if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+  }
+  if (foundInText) {
+    message.delete();
+    message.reply('Aduh dek... masih kecil gak boleh ngomong gitu. Chat kamu kakak hapus ya')
+  }
+  }
+
+})
+
 bot.login(process.env.token);
