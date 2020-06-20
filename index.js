4479b735bf3c132d90b4f8e8e973dbc954cd6c1e
fancy164 <66501889+fancy164@users.noreply.github.com>
2020-06-20 19:44:56 +0800

    penyederhanaan

--- index.js
+++ index.js
@@ -10,16 +10,6 @@ bot.on('message', message => {
 
   if (message.content === 'ping')
     message.channel.send('pong');
-  
-  let blacklisted = ['bangsat', 'bangsad', 'bgsd', 'bgst', 'bangst', 'bngsat', 'anjing', 'njing', 'anying', 'anjng', 'goblok', 'gblk'];
-  let foundInText = false;
-  for (var i in blacklisted){
-    if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
-  }
-  if (foundInText) {
-    message.delete();
-    message.reply('Aduh dek... masih kecil gak boleh ngomong gitu. Chat kamu kakak hapus ya')
-  }
 });
 
 bot.login(process.env.token);
